806b0786aea023e4c74ceb617cc81e3ea080a6a3
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-01 19:25:50 -0400

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+// ASSERT EQUALS
+const assertEqual = function(actual, expexted) {
+  if (actual === expexted) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
+};
+
+const countLetters = function(string) {
+  let letters = {};
+  // loop through string
+  for (let letter of string) {
+    // removes the spaces of the string
+    if (letter !== ' ') {
+      // if letter is captured, add 1 to our count
+      if (letters[letter]) {
+        letters[letter] += 1;
+      // if the letter doesnt exist in our our count, it creates it
+      } else {
+        letters[letter] = 1;
+      }
+    }
+  }
+  // return the result
+  return letters;
+};
+
+console.log(countLetters("lighthouse in the house"));
