bdaef6b9a5f118007002e5cc9fcd0d95b6474c37
Colleen Garvey <colleenellengarvey@gmail.com>
2022-05-31 21:05:09 -0400

    added comments

--- eqArrays.js
+++ eqArrays.js
@@ -9,13 +9,18 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("one", 1);
 
+// create a function with eqArrays with 2 parameters
 function eqArrays(arr1, arr2) {
+  // use a loop to go through each array
   for(let i = 0; i < arr1.length; i++) {
+    // use if statement to compare the two arrays given.. use [i] to loop through each array
     if(arr1[i] !== arr2[i]) {
+      // return false
       console.log("false");
       return false;
     }
   }
+  // return true
   console.log("true");
   return true;
 };
