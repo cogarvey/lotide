10443c24d9c9c938b3ede1c028c784914e0249ec
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-02 19:43:52 -0400

    created map function and compared with assert equals

---
+++ map.js
@@ -0,0 +1,53 @@
+// ASSERT EQUALS
+const assertEqual = function(actual, expexted) {
+  if (actual === expexted) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
+  }
+  
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
+};
+
+
+// EQUALS ARRAY
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// ASSERT ARRAYS EQUAL
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  assertEqual(result, true);
+};
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g','c','t','m','t']);
+console.log(results1);
+
+const results2 = map(words, word => word[1]);
+assertArraysEqual(results2, ['r','o', 'o', 'a', 'o']);
+console.log(results2);
+
+const results3 = map(words, word => word.length);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
+console.log(results3);
