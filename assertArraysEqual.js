c59fa183c4fa5778aef661a5771ac7230e7579e8
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-02 19:54:41 -0400

    updated functions accordingly -- with assistance from mentor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,34 @@
-// create a function with eqArrays with 2 parameters
+// EQUALS ARRAY
 const eqArrays = function(arr1, arr2) {
-  // use a loop to go through each array
-  for(let i = 0; i < arr1.length; i++) {
-    // use if statement to compare the two arrays given.. use [i] to loop through each array
-    if(arr1[i] !== arr2[i]) {
-      // return false
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      // console.log(false);
       return false;
     }
   }
-  // return true;
+  // console.log(true);
   return true;
 };
 
-const assertArraysEqual = function(arr1, arr2) {
-  // refer to previous eqArrays function to console log message
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+// ASSERT EQUALS
+const assertEqual = function(actual, expexted) {
+  if (actual === expexted) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
   }
+  
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
 };
+
+
+// ASSERT ARRAYS EQUAL
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  assertEqual(result, true);
+};
+
 
 // test out the function
 assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,27 @@
+// ASSERT EQUALS
 const assertEqual = function(actual, expexted) {
   if (actual === expexted) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
   }
+  
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("one", 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("one", 1);
 
-// create a function with eqArrays with 2 parameters
+// EQUALS ARRAY
 const eqArrays = function(arr1, arr2) {
-  // use a loop to go through each array
-  for(let i = 0; i < arr1.length; i++) {
-    // use if statement to compare the two arrays given.. use [i] to loop through each array
-    if(arr1[i] !== arr2[i]) {
-      // return false
-      console.log("false");
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(false);
       return false;
     }
   }
-  // return true
-  console.log("true");
+  console.log(true);
   return true;
 };
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
