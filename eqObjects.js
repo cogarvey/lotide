49820c8a999399134a2c28b90772742afe914070
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-02 16:57:34 -0400

    created eqObjects and compared to eqArrays

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,16 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
 // // ASSERT EQUALS
 const assertEqual = function(actual, expexted) {
   if (actual === expexted) {
@@ -8,23 +21,36 @@ const assertEqual = function(actual, expexted) {
 };
 
 // EQUAL OBJECTS
-const eqObjects = function(obj1, obj2) {
-  if (obj1.length !== obj2.length) {
-    return false;
-  }
-  for (let i = 0; i < obj1.length; i++) {
-    if (obj1[i] !== obj2[i]) {
-      return false;
+const eqObjects = function(object1, object2) {
+  const object1KeysArray = Object.keys(object1);
+  const object2KeysArray = Object.keys(object2);
+  let result = object1KeysArray.length === object2KeysArray.length;
+  if (result) {
+    for (let key1 of object1KeysArray) {
+      for (let key2 of object2KeysArray) {
+        if (eqArrays(object1[key1], object2[key2]) && eqArrays(key1, key2)) {
+          result = true;
+          break;
+        } else {
+          result = false;
+        }
+      }
     }
   }
-  return true;
+  return result;
 };
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); 
+assertEqual(eqObjects(ab, abc), false); // => false
 
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
