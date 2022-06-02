cbdb4f45934e803ba8e3899f8656c77037b5d92b
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-01 22:58:36 -0400

    function to find the key by value.. uses 2 parameters for object and the value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// // ASSERT EQUALS
+const assertEqual = function(actual, expexted) {
+  if (actual === expexted) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
+  }
+  
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
+};
+
+
+const findKeyByValue = function(obj, value) {
+  const keys = Object.keys(obj);
+  // console.log(obj)
+  for (let key of keys) {
+    if (obj[key] === value) 
+    return key;
+ }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
