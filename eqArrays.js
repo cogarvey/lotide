03ec0fe4145a64f7247e28aaf9821658adfd7f2b
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-06 15:13:38 -0400

    changed to include new testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,5 @@
-// ASSERT EQUALS
-const assertEqual = function(actual, expexted) {
-  if (actual === expexted) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expexted}`);
-  }
-  
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expexted}`);
-};
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("one", 1);
+const assertArraysEqual = require('./assertArraysEqual');
+
 
 // EQUALS ARRAY
 const eqArrays = function(arr1, arr2) {
@@ -17,15 +8,12 @@ const eqArrays = function(arr1, arr2) {
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(false);
+      // console.log(false);
       return false;
     }
   }
-  console.log(true);
+  // console.log(true);
   return true;
 };
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = eqArrays;
