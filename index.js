2ebc25618904a17f59f6567b4baf173081dbcef6
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-06 18:01:14 -0400

    updated to include all files

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
-
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head,
@@ -13,4 +22,14 @@ module.exports = {
   eqArrays,
   assertEqual,
   assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
